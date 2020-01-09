f83b5a840d509cfde0c070439cfeb995afcb6d3e
gorgovic <45965967+gorgovic@users.noreply.github.com>
2020-01-09 15:16:11 +0100

    Add flag to toggle color

--- rubarb.go
+++ rubarb.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	in, out, xOffset, yOffset, separation, lineWidth := parseArgs()
+	in, out, xOffset, yOffset, separation, lineWidth, color := parseArgs()
 
 	img, err := gg.LoadPNG(in)
 	if err != nil {
@@ -16,6 +16,7 @@ func main() {
 	bounds := img.Bounds()
 	dc := gg.NewContext(bounds.Dx()*separation+2*Abs(xOffset), bounds.Dy()*separation+2*Abs(yOffset))
 	dc.SetLineWidth(float64(lineWidth))
+	dc.SetRGB(1, 1, 1)
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			r, g, b, a := img.At(x, y).RGBA()
@@ -25,10 +26,13 @@ func main() {
 			ox, oy := float64(xOffset)*val, float64(yOffset)*val
 			px, py := float64(x*separation+Abs(xOffset))+ox, float64(y*separation+Abs(yOffset))+oy
 
+			if color {
+				dc.SetColor(img.At(x, y))
+			}
+
 			if x == bounds.Min.X {
 				dc.MoveTo(px, py)
 			} else {
-				dc.SetColor(img.At(x, y))
 				dc.LineTo(px, py)
 				dc.Stroke()
 				dc.ClearPath()
@@ -40,15 +44,16 @@ func main() {
 	dc.SavePNG(out)
 }
 
-func parseArgs() (string, string, int, int, int, int) {
+func parseArgs() (string, string, int, int, int, int, bool) {
 	inPtr := flag.String("i", "in.png", "input file")
 	outPtr := flag.String("o", "out.png", "output file")
 	xOffsetPtr := flag.Int("x", 0, "x offset")
 	yOffsetPtr := flag.Int("y", -20, "y offset")
 	separationPtr := flag.Int("s", 4, "line separation")
 	lineWidthPtr := flag.Int("l", 1, "line width")
+	colorPtr := flag.Bool("c", true, "use image color")
 	flag.Parse()
-	return *inPtr, *outPtr, *xOffsetPtr, *yOffsetPtr, *separationPtr, *lineWidthPtr
+	return *inPtr, *outPtr, *xOffsetPtr, *yOffsetPtr, *separationPtr, *lineWidthPtr, *colorPtr
 }
 
 func Abs(x int) int {
